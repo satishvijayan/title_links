36d3da6ea693c9cd29f33ba5c77d3ef71ba051d2
Satish VIjayan <satish@charioteersoftware.com>
2021-01-02 07:13:29 +0000

    20210102 - Setup.py

--- setup.py
+++ setup.py
@@ -8,8 +8,11 @@ except ImportError: # for pip <= 9.0.3
     from pip.req import parse_requirements
 
 version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+# requirements = parse_requirements("requirements.txt", session="")
+# https://discuss.erpnext.com/t/solved-command-python-setup-py-egg-info-failed-with-error-code-1/44487
 
+with open('requirements.txt') as f:
+	install_requires= f.read().strip().split('\n')
 setup(
 	name='title_links',
 	version=version,
@@ -19,6 +22,6 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=install_requires,
+	#dependency_links=[str(ir._link) for ir in requirements if ir._link]
 )
